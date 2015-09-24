1d5949718b8d69e4cd04280bc55ac0b7be891494
Ariana Cisneros Garza <ariana.cisneros@live.com>
2015-09-24 15:30:28 -0500

    cronometro

--- js_scripts/heartbeat.js
+++ js_scripts/heartbeat.js
@@ -1,8 +1,12 @@
+
+
+
+
 function pulsate(){
 	$('#icon').stop().animate({fontSize : '13em'},20,function(){
 		$(this).stop().animate({color : '#FE5757'});
 	}).removeClass('fa-heart').addClass('fa-heartbeat');
-	
+
 }
 
 function contract(){
@@ -16,28 +20,9 @@ var intervalIndex = 0;
 
 function beating(){
 	intervalFunctions[intervalIndex++ % 2]();
+
 }
 
 var beat = setInterval(function(){beating()}, 291.5);
+
 //se ejecutan 2 funciones con un delay de 291.5 ms entre ellas (x2 = 583 ms = 103 BPM)
-
-
-/*
-Modulus (%) - Sirve para sacar el restante de una división. En el metodo setInterval se divide intervalIndex % 2 
-debido a que esto siempre produce la...alternancia? binaria que se necesita para pasar de una función a otra en el arreglo
-intervalFunctions. Si se agregan más funciones al arreglo, es necesario dividir intervalIndex % intervalFunctions.length para
-que siempre se recorran todos los índices del arreglo.
-Ex. Cuando hay 2 funciones [0,1]
-0 % 2 = 0
-1 % 2 = 1 (No, no es .5, solo usa números enteros por lo que el 1 no se puede dividir entre 2, dejando como restante 1)
-2 % 2 = 0
-3 % 2 = 1
-4 % 2 = 0 
-
-Ex. Cuando hay 3 funciones [0,1,2]
-0 % 3 = 0 
-1 % 3 = 1
-2 % 3 = 2
-3 & 3 = 0
-4 % 3 = 1
-*/
--- js_scripts/view_controller.js
+++ js_scripts/view_controller.js
@@ -2,12 +2,117 @@ var check_inst=true;
 var victim = "";
 
 
+var compInterval;
+var comp = 0;
+var vent = 0;
+var ciclo = 0;
+var ventInterval;
+
+function playAdult(){
+	comp = 0;
+	vent = 0;
+	ciclo = 0;
+	compInterval=setInterval(function(){playCompAdulto()},583);
+}
+
+function playBebe(){
+	comp = 0;
+	vent = 0;
+	ciclo = 0;
+	compInterval=setInterval(function(){playCompBebe()},583);
+}
+
+function playCompAdulto(){
+		var audio = document.getElementById("audio");
+		audio.play();
+		comp += 1;
+
+		var vis = document.getElementById("num");
+		vis.innerHTML = "Compresión " + comp;
+
+
+		var cic = document.getElementById("cicloNum");
+		cic.innerHTML = "Ciclo " + ciclo;
+
+		if(comp >= 30){
+			ciclo += 1;
+			clearInterval(compInterval);
+			comp = 0;
+			vis.innerHTML = "Compresión " + comp;
+			ventInterval = setInterval(function(){playVentAdulto()},3000);
+
+		}
+	}
+
+function playVentAdulto(){
+	if(ciclo == 5)
+	{
+		//checar pulso
+	}
+	var audio = document.getElementById("audio");
+	audio.play();
+
+		vent+=1;
+		if(vent == 2){
+			vent = 0;
+			clearInterval(ventInterval);
+			compInterval= setInterval(function(){playCompAdulto()},583);
+		}
+
+	}
+
+function playCompBebe(){
+		var audio = document.getElementById("audio");
+		audio.play();
+		comp += 1;
+
+		var vis = document.getElementById("num");
+		vis.innerHTML = "Compresión " + comp;
+
+
+		var cic = document.getElementById("cicloNum");
+		cic.innerHTML = "Ciclo " + ciclo;
+
+
+		if(comp >= 10){
+			ciclo += 1;
+			vis.innerHTML = "Compresión " + comp;
+			clearInterval(compInterval);
+			ventInterval = setInterval(function(){playVentBebe()},5000);
+		comp = 0;
+		}
+	}
+
+function playVentBebe(){
+	if(ciclo == 10)
+	{
+		//checar pulso
+	}
+	var audio = document.getElementById("audio");
+	audio.play();
+
+		vent+=1;
+		if(vent == 2){
+			vent = 0;
+			clearInterval(ventInterval);
+			compInterval= setInterval(function(){playCompBebe()},583);
+		}
+	}
+
+
+
+
+
+
+
+
+
 ///////////CHECK VICTIM
 $('.box-body').click(function(){
 	victim = $(this).attr('id');
 	console.log(victim);
 	is_checked(victim);
-	
+
 });
 
 ///////////CHECK INSTRUCTIONS
@@ -15,12 +120,13 @@ function is_checked(victim){
 	check_inst = $('input[name=instrucciones]').is(':checked');
 	console.log(check_inst);
 	$('#section1').hide();
-	
+
 	//////NEXT SECTION
 	if(check_inst){
 		$('#section2').show(100);
 	}else{
 		$('#section3').show(100);
+		playAdult();
 		//timer(victim);
 	}
 }
@@ -34,6 +140,7 @@ function navigation(){
 	$('#2next').click(function(){
 		$('#section2').hide();
 		$('#section3').show(100);
+		playAdult();
 	});
 	$('#3back').click(function(){
 		$('#section3').hide();
